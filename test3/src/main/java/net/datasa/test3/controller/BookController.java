c9acca97ecdc73c3482bbc17a7c5e1e93bf076e7
user <user@DESKTOP-2AC9454>
2024-08-07 18:19:30 +0900

    awrg

---
+++ test3/src/main/java/net/datasa/test3/controller/BookController.java
@@ -0,0 +1,39 @@
+package net.datasa.test3.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.slf4j.Slf4j;
+import net.datasa.test3.Service.BookService;
+import net.datasa.test3.domain.dto.BookDto;
+
+@Controller
+@Slf4j
+public class BookController {
+	
+	@Autowired
+	BookService bookService;
+	
+	@PostMapping("add")
+	public String addBook(@ModelAttribute BookDto bookDto) {
+		
+		bookService.bookAdd(bookDto);
+		return "redirect:/addBook";
+	}
+	
+	@GetMapping("find")
+	public String findBook(@RequestParam(name ="ISBN") String ISBN, Model model) {
+		BookDto bookDto = bookService.bookGet(ISBN);
+		if(bookDto != null) {
+			model.addAttribute("book", bookDto);
+		}
+		model.addAttribute("ISBN", ISBN);
+		return "findBook";
+	}
+	
+}
